7ec944b4a07690d510d658a6221e611f5809e0b2
Robin Millette <robin@millette.info>
2017-12-05 09:40:56 -0500

    Use update-notifier.

--- cli.js
+++ cli.js
@@ -3,5 +3,10 @@
 // self
 const DatRepl = require('.')
 
+// npm
+const updateNotifier = require('update-notifier')
+
+updateNotifier({ pkg: require('./package.json') }).notify()
+
 const rr = new DatRepl({ key: process.argv[2] })
 rr.start()
